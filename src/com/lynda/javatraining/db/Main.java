61283be6229fa5f93c17317231890ba53659d97b
Pablo Munoz <pfariasmunoz@gmail.com>
2016-04-08 22:23:55 -0300

    initial commit

---
+++ src/com/lynda/javatraining/db/Main.java
@@ -0,0 +1,30 @@
+package com.lynda.javatraining.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+	
+	private static final String USERNAME = "dbuser";
+	private static final String PASSWORD = "iklru667";
+	private static final String CONN_STRING =
+			"jdbc:hsqldb:data/explorecalifornia";
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+			System.out.println("Connected!");
+		} catch ( SQLException e) {
+			System.err.println(e);
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+		}
+
+	}
+
+}
